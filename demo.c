75994b8f97b9276b92d89be543d106b80a5c66e5
Francisco Reyes <francisco.reyesja@sansano.usm.cl>
2020-05-24 23:42:42 -0400

    creación de procesos hijos y matriz de tiempo inicializada

--- demo.c
+++ demo.c
@@ -2,37 +2,99 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+
+void inicializacion_matriz_tiempos(int arreglo[5][5]){
+    int i, j;
+    for (i=0;i<5;i++){  
+            for(j=0;j<5;j++){
+                arreglo[i][j] = 0;  //se configura la matriz con cada celda o topo con tiempo cero, por lo que no hay ningun topo levantado
+            }
+        }
+}
+
+void lectura_matriz_tiempos(int arreglo[5][5]){
+    int i, j;
+    int valores[5];
+    for (i=0;i<5;i++){  
+            for(j=0;j<5;j++){
+                valores[j] = arreglo[i][j];
+            }
+        printf("[%d], [%d], [%d], [%d], [%d]\n", valores[0], valores[1], valores[2], valores[3], valores[4]);
+        }
+}
+
 int main(void){
-    int fd[2];
+    int p_h1[2], h1_h2[2], h2_h3[2], h3_p[2];       //pipes
     char buf[100];
     int num;
-    pid_t pid;
+    pid_t pid_p, pid_h1, pid_h2, pid_h3, pid;            //pids
 
-    pipe(fd);
+    pipe(p_h1);
+    pipe(h1_h2);
+    pipe(h2_h3);
+    pipe(h3_p);
 
-    switch (pid = fork())
+    switch (pid = fork())       //creacion de hijos 1, 2 y 3
     {
-    case 0:         //hijo
-        close(fd[0]);
-        write(fd[1], "aloha!", 6);
-        write(fd[1], " adios", 6);
-        close(fd[1]);
-        exit(0);
+    case 0:         //hijo 1
+        close(h1_h2[0]);       //cerramos lectura del hijo 1 a hijo 2
+        close(p_h1[1]);         //cerramos escritura de hijo 1 a padre
+        pid_h1 = getpid();      //almaceno pid hijo 1
         break;
     
     case -1:
         /*no funciona*/
     
     default:     //padre
-        printf("%d, y el del padre %d\n", pid, getpid());
-        close(fd[1]);
-        while(0 < (num = read(fd[0], buf , 6))){
-            buf[num] = 0;
-            printf("El padre recibe %d bytes, %s \n", num, buf);
+        //printf("%d, y el del padre %d\n", pid, getpid());
+        close(h3_p[1]);     //cerramos escritura de padre a hijo 3
+        close(p_h1[0]);     //cerramos lectura de padre a hijo 1
+        pid_p = getpid();   //almaceno pid padre
+        switch (pid = fork())
+        {
+        case 0:         //hijo 2
+            close(h2_h3[0]);       //cerramos lectura del hijo 2 a hijo 3
+            close(h1_h2[1]);         //cerramos escritura de hijo 2 a hijo 1
+            pid_h2 = getpid();      //almaceno pid hijo 2
+            break;
+        
+        case -1:
+            /*no funciona*/
+        
+        default:     //padre
+            switch (pid = fork())
+            {
+            case 0:     //hijo 3
+                pid_h3 = getpid();
+                close(h3_p[0]);     //cerramos lectura de hijo 3 a padre
+                close(h2_h3[1]);    //cerramos escritura de hijo 3 a hijo 2
+                break;
+            case -1:
+                //no funciona 
+                break;
+            
+            default:        //padre
+                sleep(1);
+                break;
+            }
+            break;
         }
-        printf("El padre va a terminar ya ...\n");
-        close(fd[0]);
+        
         break;
     }
+
+    //creacion de matriz de tiempo de los topos solo para el padre
+    //int i, j;
+    if (getpid() == pid_p){
+        int arreglo_tiempos[5][5];      //arreglo de 5x5 que almacena el estado de cada celda del tablero con tiempos de cada topo 
+        inicializacion_matriz_tiempos(arreglo_tiempos);
+        lectura_matriz_tiempos(arreglo_tiempos);
+    }
+    printf("buena soy el proceso %d\n", getpid());
+
+    //aqui empieza el juego 
+
+
     return 0;
 }
